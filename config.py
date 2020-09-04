1f049313725dca4f599e7c89aff9f380ac413af1
lolpirate <debamalyadawn21@gmail.com>
2020-09-04 22:58:54 +0530

    Updates for Heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "thisismyveryveryverysecretkey"
-    DATABASE_URL = os.environ.get("DATABASE_URI") or r"sqlite:///db.sqlite3"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or r"sqlite:///db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS") or True
 
 
